bf6b381f6f7eb34c87c606c72ed6cfd384a02541
xhh <jiatangxi@qq.com>
2026-04-25 22:02:11 +0800

    fix: 修复一些配置

--- src/configuration.ts
+++ src/configuration.ts
@@ -4,8 +4,8 @@ import * as yml from 'js-yaml';
 console.log(`${process.env.NODE_ENV}`);
 
 const envFilePath = path.join(
-  __dirname,
-  `../../config/config.${process.env.NODE_ENV || 'development'}.yml`,
+  process.cwd(),
+  `config/config.${process.env.NODE_ENV || 'development'}.yml`,
 );
 const fileContent = fs.readFileSync(envFilePath, 'utf-8');
 const config = () => yml.load(fileContent) as Record<string, any>;
